8e4644e92936974fd02689e6782ef67b6f6a1901
ravelindev <ravelindev@gmail.com>
2023-05-27 21:32:20 -0700

    Done with the initial setup for this projetc

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
@@ -26,4 +34,9 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
 })
